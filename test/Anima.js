17f64294fcacb9a37c106b0895f62d5cea216b5a
Ronald Casili <nvlled@base12>
2017-08-28 08:00:14 -0400

    Add dumb, broken tests

---
+++ test/Anima.js
@@ -0,0 +1,10 @@
+
+let Anima = require("src/Anima");
+var assert = require('assert');
+
+describe('Anima', function() {
+    describe('#apply()', function() {
+    });
+});
+
+
---
+++ test/Vec.js
@@ -0,0 +1,14 @@
+
+let Vec = require("src/Vec");
+var assert = require('assert');
+
+describe('Vec', function() {
+    describe('test()', function() {
+        it('asjdifoasdjf', function() {
+            let v = Vec.new({x: 5, y: 0});
+            //console.log(v.mul(0.5));
+        });
+    });
+});
+
+
