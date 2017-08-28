1331c2e543f3915245617d54a83fcf9e33af05f9
Ronald Casili <nvlled@base12>
2017-08-28 07:55:36 -0400

    Implement checkers game

--- src/Checkers.js
+++ src/Checkers.js
@@ -1,32 +1,56 @@
 
 let Actions = require("src/Actions");
+let Anima = require("src/Anima");
 let Waypoint = require("src/Waypoint");
 let Util = require("src/Util");
 let Vec = require("src/Vec");
 let Grid = require("src/Grid");
 let Block = require("src/Block");
 let Keyboard = require("src/Keyboard");
+let EasingFn = require("src/EasingFn");
 let PIXI = require("pixi.js");
+
+let GridTiles = require("src/GridTiles");
+
+let TEAM = Symbol();
+let TEAM1 = Symbol();
+let TEAM2 = Symbol();
 
 let M = {
     create({
         algebra,
-        rows=6,
-        cols=4,
+        rows=8,
+        cols=8,
         tileSize,
         tileSpace,
-        tileMap,
+        //tileMap,
         x, y,
         speed=900,
         stretch=.8,
-        interactive,
+        //interactive,
         alpha,
 
         onGameOver=()=>{},
     } = {}) {
+        let {randomTexture} = GridTiles;
+        let tile1 = randomTexture(PIXI.loader.resources);
+        let tile2 = randomTexture(PIXI.loader.resources, new WeakSet([tile1]));
+
+        let tileMap = (x, y) => {
+            if (y%2 == 0) { 
+                if (x%2 == 0)
+                    return tile1;
+                return tile2;
+            } else {
+                if (x%2 == 0)
+                    return tile2;
+                return tile1;
+            }
+        }
+
         let grid = Grid.new({
             x, y, rows, cols, tileSize, tileSpace, tileMap,
-            speed, stretch, interactive, alpha,
+            speed, stretch, interactive: true, alpha,
         });
         grid.setInteractive();
         
@@ -52,17 +76,100 @@ let M = {
         return self;
     },
 
-    init(self) {
+    async init(self) {
         self.grid.onTileClick = (pos) => {
-
+            for (let p of points) {
+                self.grid.tileAt(p).tint = 0xffffff;
+            }
         }
 
-        //self.grid.hightlightTiles([
-        //        {x: 0, y:0},
-        //        {x: 1, y:0},
-        //        {x: 0, y:1},
-        //], 0xff0000);
-        //self.grid.hightlightRange({x: 0, y: 2}, {x: 3, y: 2});
+        let srcPos = null;
+        let dstPos = null;
+        let sprite = null;
+        let points = [];
+        self.grid.onTileDown = (pos) => {
+            sprite = self.grid.spriteAt(pos);
+            if (!sprite)
+                return;
+            points = M.listNextPositions(self, pos);
+            for (let p of points) {
+                self.grid.tileAt(p).tint = 0xff0000;
+            }
+            srcPos = pos;
+        }
+        self.grid.onTileDrag = async (pos, __) => {
+            for (let p of points) {
+                self.grid.tileAt(p).tint = 0xffffff;
+            }
+            if (Vec.equals(srcPos, dstPos)) {
+                return;
+            }
+            let dir = Vec.new(dstPos).sub(srcPos);
+            let pos_ = Vec.new(pos).add(Vec.new(dir).idiv(2));
+            let sprite_ = self.grid.spriteAt(pos_);
+            if (sprite_ && Vec.dist(dstPos, srcPos) == 4) {
+
+                let zSprite = self.algebra.applySprites(sprite, sprite_);
+                self.grid.setSprite({sprite, x: srcPos.x, y: srcPos.y});
+                if (zSprite) {
+                    Anima.boom(sprite_);
+                    await self.grid.move({src: srcPos, dest: dstPos});
+                    self.grid.removeSprite(pos_);
+                    self.grid.removeSprite(srcPos);
+                    self.grid.setSprite({sprite: zSprite, x: dstPos.x, y: dstPos.y});
+                    Anima.fade(sprite, {end: 0});
+                    await Anima.fade(zSprite, {end: 1, start: 0});
+                    sprite_.destroy();
+                    sprite.destroy();
+                } else {
+                    let pos = Vec.new(sprite.position);
+                    await Anima.move(sprite, {end: sprite_.position, seconds: 0.1});
+                    await Anima.move(sprite, {end: pos, seconds: 0.1});
+                }
+            }
+        }
+        self.grid.onTileDragging = (pos, pos_) => {
+            if (!sprite || !pos_)
+                return;
+            for (let p of points) {
+                if (Vec.equals(p, pos_)) {
+                    self.grid.setSprite({sprite, x: pos_.x, y: pos_.y});
+                    dstPos  = pos_;
+                    break;
+                }
+            }
+
+            //let sprite = self.algebra.randomSprite();
+            //self.grid.setSprite({sprite, x: pos.x, y: pos.y});
+            //Anima.fade(sprite, {
+            //    end: 1, start: 0,
+            //    seconds: 3,
+            //});
+        }
+    },
+
+    listNextPositions(self, pos) {
+        let {grid} = self;
+        let dirs = [[1, 1], [1, -1], [-1, 1], [-1, -1]];
+        let points = [];
+        let sprite = grid.spriteAt(pos);
+        if (!sprite)
+            return [];
+        for (let arr of dirs) {
+            let dir = Vec.new({x: arr[0], y: arr[1]});
+            let p1 = Vec.new(pos).add(dir);
+            let p2 = Vec.new(pos).add(Vec.mul(dir, 2));
+            if (!grid.hasSprite(p1))
+                points.push(p1);
+            else {
+                let sprite_ = grid.spriteAt(p1);
+                if (!grid.hasSprite(p2) && grid.hasSprite(p1) &&
+                        sprite[TEAM] != sprite_[TEAM])
+                    points.push(p2);
+            }
+        }
+        points.push(pos);
+        return points;
     },
 
     // TODO:
@@ -107,7 +214,7 @@ let M = {
         M.init(self);
         //M.listenKeys(self);
         //self.actions.start();
-        self.randomize(5);
+        self.randomize();
     },
 
     stop(self) {
@@ -140,36 +247,26 @@ let M = {
         let filled = {};
         let retries = 128;
 
-
         let elems = algebra.getElems();
-        Util.remove(elems, algebra.algebra.identity);
-        Util.shuffle(elems);
-        let count = elems.length;
 
         self.fixed = {};
-        for (n = 0; n < count; n++) {
+        let rows = (grid.rows/2)-1;
+        let cols = grid.cols;
 
-            let elem = elems[n];
-
-            let [_, i] = Util.randomSelect(grid.tiles, filled, false);
-            if (i == null)
-                return false;
-
-            filled[i] = true;
-            filled[elem] = true;
-            self.fixed[i] = true;
-            let {x, y} = grid.toXY(i);
-            if (self.grid.hasSprite({x, y})) {
-                n--;
-                retries--;
-                if (retries <= 0)
-                    break;
-            } else {
-                let sprite = algebra.createSprite(elem);
-                grid.setSprite({x, y, sprite});
+        let fill = (startRow, team) => {
+            for (let y = startRow; y < startRow+rows; y++) {
+                let x = 0;
+                if (y%2 != 0)
+                    x++;
+                for (; x < cols; x+=2) {
+                    let sprite = algebra.randomSprite(false);
+                    sprite[TEAM] = team;
+                    grid.setSprite({sprite, x, y});
+                }
             }
         }
-        return n == count;
+        fill(0, TEAM1);
+        fill(grid.rows-rows, TEAM2);
     },
 
     combineElements(self) {
