328717f7aee86953aa780311e04fa8631977b9b1
Ronald Casili <nvlled@gmail.com>
2017-11-28 13:02:23 +0800

    Add image resources

--- src/client/algebra/Rsrc.js
+++ src/client/algebra/Rsrc.js
@@ -6,38 +6,64 @@ let {Texture, Sprite} = PIXI;
 let txtr = filename => Texture.from("static/images/"+filename);
 
 let cakeTxtr = TextureSet.new({
-    cols: 5, 
-    rows: 9, 
-    tileWidth: 64, 
-    tileHeight: 64, 
+    cols: 5,
+    rows: 9,
+    tileWidth: 64,
+    tileHeight: 64,
     exclude: 1,
     image: txtr("cake.png")
 });
 
 let konettTxtr = TextureSet.new({
-    cols: 5, 
-    rows: 6, 
-    tileWidth: 64, 
-    tileHeight: 64, 
+    cols: 5,
+    rows: 6,
+    tileWidth: 64,
+    tileHeight: 64,
     exclude: 2,
     image: txtr("konett.png"),
 });
 
+let sharm = {
+    tiles() {
+        let t = TextureSet.new({
+            cols: 8,
+            rows: 10,
+            tileWidth: 16,
+            tileHeight: 16,
+            image: txtr("sharm/tiles.png"),
+        });
+        return t;
+    },
+    characters() {
+        let t = TextureSet.new({
+            cols: 12,
+            rows: 8,
+            tileWidth: 16,
+            tileHeight: 16,
+            image: txtr("sharm/characters.png"),
+        });
+        return t;
+    },
+    charSprite() {
+    },
+}
+
 let M = {
     cake: () => cakeTxtr.textures.map(t=>new Sprite(t)),
     konett: () => konettTxtr.textures.map(t=>new Sprite(t)),
+
+    sharm,
+    numbers: () => {
+        let t = TextureSet.new({
+            cols: 5,
+            rows: 2,
+            tileWidth: 155.6,
+            tileHeight: 175.5,
+            image: txtr("numbers_set.png"),
+        });
+        return t;
+    },
 }
 
 module.exports = M;
 
-//let sprite = AniSprite.new(Object.assign({
-//    textureSet: textureSet,
-//    states: {
-//        idle: [7],
-//        left: [9,10,11],
-//        down: [6,7,8],
-//        right: [3,4,5],
-//        up: [0,1,2],
-//    },
-//    animationSpeed: 0.05
-//}, args));
