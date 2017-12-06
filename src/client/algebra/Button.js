3d0ce040a5f916494d9afe89b59f0d26b878f43e
Ronald Casili <nvlled@gmail.com>
2017-12-06 22:20:50 +0800

    Clean up Button code

--- src/client/algebra/Button.js
+++ src/client/algebra/Button.js
@@ -2,125 +2,6 @@ let Layout = require("src/client/algebra/Layout");
 let PIXI = require("src/client/pixi");
 let Util = require("src/client/algebra/Util");
 
-//let M = {
-//    create({
-//        text,
-//        width,
-//        height,
-//        bgimage,
-//        padding=15,
-//        alpha=0.9,
-//
-//        pointerdown=()=>{},
-//        pointerup=()=>{},
-//        press=()=>{},
-//        
-//        fgStyle={
-//            normal: 0xffffff,
-//            hover: 0xaaffaa,
-//            click: 0xaaffff,
-//        },
-//        bgStyle={
-//            normal: 0x660000,
-//            hover: 0xbb0000,
-//            click: 0xdd0000,
-//        }
-//    }) {
-//        if (pointerup && !press)
-//            press = pointerup;
-//        else if (!pointerup && press)
-//            pointerup = press;
-//
-//        let container = new PIXI.Container();
-//        let buttonText = new PIXI.Text(text, {
-//            fill: fgStyle.normal,
-//            align: "center",
-//        });
-//
-//        let bounds = buttonText.getBounds();
-//        let pad = padding;
-//        let rect = new PIXI.Graphics();
-//
-//        if (width == null)
-//            width = bounds.width;
-//        if (height == null)
-//            height = bounds.height;
-//
-//        let drawRect = (state="normal") => {
-//            rect.beginFill(bgStyle[state], alpha);
-//            //rect.drawRoundedRect(bounds.x-pad/2, bounds.y-pad/2, 
-//            //        bounds.width+pad, bounds.height+pad, 10);
-//            rect.drawRoundedRect(0, 0,
-//                    width+pad, height+pad, 10);
-//            rect.endFill();
-//        }
-//        drawRect();
-//
-//        let hovered = false;
-//        //buttonText.interactive = true;
-//        //buttonText.buttonMode = true;
-//        rect.interactive = true;
-//        rect.buttonMode = true;
-//
-//        rect.on("pointerover", () => {
-//            hovered = true;
-//            buttonText.style.fill = fgStyle.hover;
-//            drawRect("hover");
-//        });
-//        rect.on("pointerout", () => {
-//            hovered = false;
-//            buttonText.style.fill = fgStyle.normal;
-//            drawRect("normal");
-//        });
-//        rect.on("pointerdown", () => {
-//            pointerdown();
-//            buttonText.style.fill = fgStyle.click;
-//            drawRect("click");
-//        });
-//        rect.on("pointerup", () => {
-//            pointerup();
-//
-//            if (hovered) {
-//                buttonText.style.fill = fgStyle.hover;
-//                drawRect("hover");
-//            } else {
-//                buttonText.style.fill = fgStyle.normal;
-//                drawRect("normal");
-//            }
-//        });
-//        container.setSize = function(args) {
-//            if (args.width) 
-//                 width =  args.width+padding;
-//            if (args.height)
-//                 height =  args.height+padding;
-//            drawRect();
-//            Layout.centerOf({}, rect, buttonText);
-//        }
-//
-//        container.addChild(rect);
-//        container.addChild(buttonText);
-//        container.setSize({width, height});
-//
-//        return new Proxy(container, {
-//            set(target, name, value) {
-//                if (name == "alpha") {
-//                    container.alpha = value;
-//                    rect.alpha = value;
-//                    buttonText.alpha = value;
-//                } else if (name == "width") {
-//                    //rect.width = value;
-//                    container.setSize({width: value});
-//                } else if (name == "height") {
-//                    container.setSize({height: value});
-//                } else {
-//                    target[name] = value;
-//                }
-//                return true;
-//            },
-//        });
-//    },
-//}
-
 let defaultHandler = btnText => { };
 
 let M = {
@@ -133,7 +14,6 @@ let M = {
         width,
         height,
         bgimage,
-        padding=8,
         alpha=0.7,
         image="",
         fitImage=false,
@@ -141,7 +21,7 @@ let M = {
         pointerdown=defaultHandler,
         pointerup=defaultHandler,
         press=defaultHandler,
-        
+
         fgStyle={},
         bgStyle={},
 
@@ -164,7 +44,7 @@ let M = {
 
         //if (pointerup && !press)
         //    press = pointerup;
-        if (pointerup == defaultHandler && 
+        if (pointerup == defaultHandler &&
             press != defaultHandler) {
             pointerup = press;
         }
@@ -194,14 +74,13 @@ let M = {
         let bgImage = PIXI.Sprite.fromImage(image);
 
         let bounds = buttonText.getBounds();
-        let pad = padding;
         let rect = new PIXI.Graphics();
         let bgImageMask = new PIXI.Graphics();
 
         if (width == null)
-            width = bounds.width;
+            width = bounds.width*1.5;
         if (height == null)
-            height = bounds.height;
+            height = bounds.height*1.5;
 
         bgImage.width = width;
         bgImage.height = height;
@@ -263,10 +142,10 @@ let M = {
             }
         });
         container.setSize = function(args) {
-            if (args.width) 
-                 width =  args.width+padding*2;
+            if (args.width)
+                 width =  args.width;
             if (args.height)
-                 height =  args.height+padding*2;
+                 height =  args.height;
             drawRect();
             Layout.centerOf({}, rect, buttonText);
         }
